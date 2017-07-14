f1aa4516fba64fc70a7790bace5b5e04a815ca7b
Jori Ostrovskij <jori.ostrovskij@granlund.fi>
2017-07-14 16:36:25 +0300

    Talletus kantaan asti. Submitin jälkeen lomakkeen tyhjennys ja datojen uusi haku?

--- api/insertReport.php
+++ api/insertReport.php
@@ -4,7 +4,7 @@
     $obj = json_decode($_POST["data"], true);
 
     $sql = "INSERT INTO raportti (koti, vieras, paikka, pvm, pt_id, vt_id, tark_id)
-            VALUES ('".$obj['koti']."', '".$obj['vieras']."', '".$obj['paikka']."', '2017-07-20 01:13:14', '".$obj['pt_id']."', '".$obj['vt_id']."', '".$obj['tark_id']."')";
+            VALUES ('".$obj['koti']."', '".$obj['vieras']."', '".$obj['paikka']."', '".$obj['pvm']."', '".$obj['pt_id']."', '".$obj['vt_id']."', '".$obj['tark_id']."')";
 
     if ($mysqli->query($sql) === TRUE) {
         $report_id = $mysqli->insert_id;
@@ -13,15 +13,11 @@
                 VALUES (".$obj['aihe_1_id'].", ".$obj['aihe_1_arvosana'].", ".$report_id.", ''),
                        (".$obj['aihe_2_id'].", ".$obj['aihe_2_arvosana'].", ".$report_id.", '')
                 ";
-        // $sql = "INSERT INTO rivi (aihe_id, arvosana, raportti_id, huom)
-        //         VALUES (1, 4, ".$report_id.", ''),
-        //                (2, 3, ".$report_id.", '')
-        //         ";
         $mysqli->query($sql);
         echo json_encode($obj);
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
-    $conn->close();
+    $mysqli->close();
 ?>

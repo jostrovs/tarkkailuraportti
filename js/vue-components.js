f98723d1db36a3c8413a0ce76958b764de641383
Jori Ostrovskij <jori.ostrovskij@granlund.fi>
2017-08-10 13:57:11 +0300

    Tyylittelyä

--- js/vue-components.js
+++ js/vue-components.js
@@ -1,15 +1,19 @@
 Vue.component('vue-rivi', {
                 template: `
                     <div class="form-group row">
-                        <label class="col-xs-4">{{initialRivi.otsikko}}</label>
-                        <div class="col-xs-3">
+                        <label class="col-xs-3">{{initialRivi.otsikko}}</label>
+                        <div class="col-xs-3" style="max-width: 230px;">
                             <label class="radio-inline"><input :id="inputId('1')" @change="onInput()" required type="radio" :name="radioname" value="1">1</label>
                             <label class="radio-inline"><input :id="inputId('2')" @change="onInput()" required type="radio" :name="radioname" value="2">2</label>
                             <label class="radio-inline"><input :id="inputId('3')" @change="onInput()" required type="radio" :name="radioname" value="3">3</label>
                             <label class="radio-inline"><input :id="inputId('4')" @change="onInput()" required type="radio" :name="radioname" value="4">4</label>
                             <label class="radio-inline"><input :id="inputId('5')" @change="onInput()" required type="radio" :name="radioname" value="5">5</label>
                         </div>
-                        <div class="col-xs-3">
+                        <div class="col-xs-4">
+                            <input class="form-control" v-model="rivi.huom" type="text">
+                        </div>
+
+                        <div class="col-xs-1">
                             Edelliset: <span v-for="vanha_rivi in rivi.vanhat_rivit">{{vanha_rivi.arvosana}} </span>
                         </div>
                         <span v-if="jos" style="margin-left: 23px;"> (valittu: {{valittu}})</span>

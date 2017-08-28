4ec4b695dc09c11140ced3358eebffb69a07d72b
Jori Ostrovskij <jori.ostrovskij@granlund.fi>
2017-08-28 20:54:16 +0300

    Pisteytys

--- js/script.js
+++ js/script.js
@@ -7,6 +7,43 @@ const API_HAE_RAPORTIN_RIVIT = 5;
 const API_HAE_PT_RAPORTIT = 6;
 const API_HAE_VT_RAPORTIT = 7;
 
+var kertoimet = [];
+kertoimet["1"]  = [20000, 5,  0,  -5, -10, -20, -100];
+kertoimet["2"]  = [20000, 2,  0,  -5, -10, -30, -100];
+kertoimet["3"]  = [20000, 2,  0, -10, -20, -40, -100];
+kertoimet["4"]  = [20000, 2,  0,  -5, -10, -30, -100];
+kertoimet["5"]  = [20000, 2,  0,  -5, -10, -30, -100];
+kertoimet["6"]  = [20000, 10, 0, -10, -30, -60, -400];
+kertoimet["7"]  = [20000, 10, 0, -10, -30, -60, -400];
+kertoimet["8"]  = [20000,  5, 0,  -5, -20, -60, -400];
+kertoimet["9"]  = [20000,  5, 0,  -5, -10, -30, -400];
+kertoimet["10"] = [20000,  5, 0,  -5, -10, -20, -100];
+kertoimet["11"] = [20000, 10, 0, -10, -30, -60, -400];
+kertoimet["12"] = [20000,  5, 0,  -5, -10, -30, -400];
+kertoimet["13"] = [20000,  5, 0,  -5, -10, -30, -100];
+kertoimet["14"] = [20000,  5, 0,  -5, -10, -20, -100];
+kertoimet["15"] = [20000, 10, 0, -10, -20, -50, -400];
+kertoimet["16"] = [20000, 10, 0,  -5, -20, -40, -100];
+kertoimet["17"] = [20000, 10, 0, -10, -20, -40, -100];
+
+kertoimet["101"] = [20000,  5,  0,  -5, -10, -20, -100];
+kertoimet["102"] = [20000,  2,  0, -10, -30, -60, -400];
+kertoimet["103"] = [20000,  2,  0,  -5, -10, -30, -100];
+kertoimet["104"] = [20000, 10,  0, -10, -20, -40, -100];
+kertoimet["105"] = [20000,  2,  0,  -5, -10, -30, -100];
+kertoimet["106"] = [20000, 10,  0,  -5, -10, -30, -100];
+kertoimet["107"] = [20000, 10, 0, -10, -30, -60, -400];
+kertoimet["108"] = [20000, 10, 0, -10, -30, -60, -400];
+kertoimet["109"] = [20000,  5, 0,  -5, -20, -60, -400];
+kertoimet["110"] = [20000,  5, 0,  -5, -10, -30, -400];
+kertoimet["111"] = [20000,  5, 0, - 5, -10, -20, -100];
+kertoimet["112"] = [20000,  5, 0,  -5, -10, -30, -400];
+kertoimet["113"] = [20000,  5, 0,  -5, -10, -30, -100];
+kertoimet["114"] = [20000,  5, 0,  -5, -10, -20, -100];
+kertoimet["115"] = [20000, 10, 0, -10, -20, -50, -400];
+kertoimet["116"] = [20000, 10, 0,  -5, -20, -40, -100];
+kertoimet["117"] = [20000, 10, 0, -10, -20, -40, -100];
+
 var localGetData=function(cmd, callback, arg1) {
     $.ajax({
         dataType: 'json',
@@ -99,6 +136,8 @@ class Rivi {
 
 class Raportti {
     constructor(data_item){
+        this.pt_score = -1;
+        this.vt_score = -1;
         if(data_item != null){
             this.id = data_item.id;
             this.koti = data_item.koti;
@@ -131,12 +170,37 @@ class Raportti {
             this.tark_nimi = '';
         }
         this.rivit = [];
+        this.laske();
     }
 
     palautaRivit(firstNo, lastNo){
         return this.rivit.filter(rivi => rivi.aihe_no >= firstNo && rivi.aihe_no <= lastNo);
     }
 
+    laske(){
+        let pt_score = 950;
+        let vt_score = 950;
+        for(let rivi of this.rivit){
+            let index = rivi.arvosana;
+            if(index == undefined) index = 2;
+            let k_rivi = kertoimet[rivi.aihe_no];
+            if(k_rivi==undefined){
+                let breaker=0;
+            }
+            let k_a = k_rivi[index];
+            
+            if(rivi.aihe_no < 100){
+                pt_score += k_a;
+            } else {
+                vt_score += k_a;
+            }
+        }
+
+        this.pt_score = pt_score/10;
+        this.vt_score = vt_score/10;
+        if(this.rivit.length < 1 || this.pt_score > 100) this.pt_score = "<puuttuu>";
+        if(this.rivit.length < 1 || this.vt_score > 100) this.vt_score = "<puuttuu>";
+    }
 
     getRivit(){
         let self = this;
--- js/vue-components.js
+++ js/vue-components.js
@@ -1,6 +1,7 @@
 Vue.component('vue-edellinen-label', {
                 template: `
                     <div :id="randomId" class="label" data-toggle="tooltip" :title="html">
+                        <span v-if="initialRivi.arvosana=='6'" class="label-dark-red">{{rivi.arvosana}}</span>
                         <span v-if="initialRivi.arvosana=='5'" class="label-red">{{rivi.arvosana}}</span>
                         <span v-if="initialRivi.arvosana=='4'" class="label-orange">{{rivi.arvosana}}</span>
                         <span v-if="initialRivi.arvosana=='3'" class="label-yellow">{{rivi.arvosana}}</span>
@@ -34,12 +35,13 @@ Vue.component('vue-rivi-edit', {
                             <span class="rivi-label">{{initialRivi.otsikko}}</span>
                             <template v-if="initialRivi.tekstiDisplayed()"><br>{{initialRivi.teksti}}</template>
                         </div>
-                        <div class="col-xs-3" style="max-width: 230px; min-width: 230px;">
+                        <div class="col-xs-3" style="max-width: 260px; min-width: 260px;">
                             <div class="radio-div ruutu1">   <label class="radio-inline"><input :id="inputId('1')" @change="onInput()" required type="radio" :name="radioname" value="1">1</label>   </div>
                             <div class="radio-div ruutu2">   <label class="radio-inline"><input :id="inputId('2')" @change="onInput()" required type="radio" :name="radioname" value="2">2</label>   </div>
                             <div class="radio-div ruutu3">   <label class="radio-inline"><input :id="inputId('3')" @change="onInput()" required type="radio" :name="radioname" value="3">3</label>   </div>
                             <div class="radio-div ruutu4">   <label class="radio-inline"><input :id="inputId('4')" @change="onInput()" required type="radio" :name="radioname" value="4">4</label>   </div>
-                            <div class="radio-div ruutu5" style="border-right: 0;">   <label class="radio-inline"><input :id="inputId('5')" @change="onInput()" required type="radio" :name="radioname" value="5">5</label>   </div>
+                            <div class="radio-div ruutu5">   <label class="radio-inline"><input :id="inputId('5')" @change="onInput()" required type="radio" :name="radioname" value="5">5</label>   </div>
+                            <div class="radio-div ruutu6" style="border-right: 0;">   <label class="radio-inline"><input :id="inputId('6')" @change="onInput()" required type="radio" :name="radioname" value="6">6</label>   </div>
                         </div>
                         <div class="col-xs-4">
                             <input class="form-control" 
@@ -54,7 +56,7 @@ Vue.component('vue-rivi-edit', {
                         <span v-if="jos" style="margin-left: 23px;"> (valittu: {{valittu}})</span>
                     </div>
                 `,
-                props: ['rivi', 'jos'],
+                props: ['raportti', 'rivi', 'jos'],
                 data: function () {
                     return {
                         randomId: this._uid,
@@ -69,6 +71,7 @@ Vue.component('vue-rivi-edit', {
                         let val = $(`input[name=${this.radioname}]:checked`).val();
                         this.rivi.arvosana = val;
                         this.valittu = val;
+                        this.raportti.laske();
                     },
                     inputId: function(no){
                         return this.rivi.aihe_no + '_' + no;
@@ -117,6 +120,7 @@ Vue.component('vue-rivi', {
                             <div class="ruutu ruutu3"> <span v-if="initialRivi.arvosana=='3'">X</span> <span v-else>&nbsp;</span> </div>
                             <div class="ruutu ruutu4"> <span v-if="initialRivi.arvosana=='4'">X</span> <span v-else>&nbsp;</span> </div>
                             <div class="ruutu ruutu5"> <span v-if="initialRivi.arvosana=='5'">X</span> <span v-else>&nbsp;</span> </div>
+                            <div class="ruutu ruutu6"> <span v-if="initialRivi.arvosana=='6'">X</span> <span v-else>&nbsp;</span> </div>
                         </div>
 
                         <div class="col-xs-3" v-if="initialRivi.huomDisplayed()">
@@ -153,32 +157,33 @@ Vue.component('vue-raportti', {
                         Tarkkailija: {{raportti.tark_nimi}}</p>
 
                         <h2>Päätuomari</h2>
+                        <p>Pisteet: {{raportti.pt_score}}</p>
                         <div class="panel-group">
                             <div class="panel panel-primary">
                                 <div class="panel-heading">Tuomaritekniikka ja suoritustaito</div>
                                 <div class="panel-body">
-                                    <vue-rivi v-for="rivi in raportti.palautaRivit(1,5)" :key="rivi.id" :rivi="rivi" :jos="jos" :tila="'pieni'"></vue-rivi>
+                                    <vue-rivi v-for="rivi in raportti.palautaRivit(1,5)" :key="rivi.id" :raportti:="raportti" :rivi="rivi" :jos="jos" :tila="'pieni'"></vue-rivi>
                                 </div>
                             </div>
                         
                             <div class="panel panel-primary">
                                 <div class="panel-heading">Sääntöjen sekä ohjeiden ja tulkintojen soveltaminen</div>
                                 <div class="panel-body">
-                                    <vue-rivi v-for="rivi in raportti.palautaRivit(6,10)" :key="rivi.id" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
+                                    <vue-rivi v-for="rivi in raportti.palautaRivit(6,10)" :key="rivi.id" :raportti:="raportti" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
                                 </div>
                             </div>
 
                             <div class="panel panel-primary">
                                 <div class="panel-heading">Vuorovaikutus joukkueiden kanssa</div>
                                 <div class="panel-body">
-                                    <vue-rivi v-for="rivi in raportti.palautaRivit(11,13)" :key="rivi.id" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
+                                    <vue-rivi v-for="rivi in raportti.palautaRivit(11,13)" :key="rivi.id" :raportti:="raportti" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
                                 </div>
                             </div>
 
                             <div class="panel panel-primary">
                                 <div class="panel-heading">Ottelun johtaminen ja persoonallisuus</div>
                                 <div class="panel-body">
-                                    <vue-rivi v-for="rivi in raportti.palautaRivit(14,17)" :key="rivi.id" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
+                                    <vue-rivi v-for="rivi in raportti.palautaRivit(14,17)" :key="rivi.id" :raportti:="raportti" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
                                 </div>
                             </div>
 
@@ -191,33 +196,34 @@ Vue.component('vue-raportti', {
                         </div>
 
                         <h2>Verkkotuomari</h2>
+                        <p>Pisteet: {{raportti.vt_score}}</p>
                         <div class="panel-group">
 
                             <div class="panel panel-primary">
                                 <div class="panel-heading">Tuomaritekniikka ja suoritustaito</div>
                                 <div class="panel-body">
-                                    <vue-rivi v-for="rivi in raportti.palautaRivit(101,106)" :key="rivi.id" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
+                                    <vue-rivi v-for="rivi in raportti.palautaRivit(101,106)" :key="rivi.id" :raportti:="raportti" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
                                 </div>
                             </div>
                             
                             <div class="panel panel-primary">
                                 <div class="panel-heading">x</div>
                                 <div class="panel-body">
-                                    <vue-rivi v-for="rivi in raportti.palautaRivit(107,111)" :key="rivi.id" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
+                                    <vue-rivi v-for="rivi in raportti.palautaRivit(107,111)" :key="rivi.id" :raportti:="raportti" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
                                 </div>
                             </div>
 
                             <div class="panel panel-primary">
                                 <div class="panel-heading">Vuorovaikutus joukkueiden kanssa</div>
                                 <div class="panel-body">
-                                    <vue-rivi v-for="rivi in raportti.palautaRivit(112,113)" :key="rivi.id" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
+                                    <vue-rivi v-for="rivi in raportti.palautaRivit(112,113)" :key="rivi.id" :raportti:="raportti" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
                                 </div>
                             </div>
 
                             <div class="panel panel-primary">
                                 <div class="panel-heading">Ottelun johtaminen ja persoonallisuus</div>
                                 <div class="panel-body">
-                                    <vue-rivi v-for="rivi in raportti.palautaRivit(114,117)" :key="rivi.id" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
+                                    <vue-rivi v-for="rivi in raportti.palautaRivit(114,117)" :key="rivi.id" :raportti:="raportti" :rivi="rivi" :tila="'pieni'" :jos="jos"></vue-rivi>
                                 </div>
                             </div>
 
